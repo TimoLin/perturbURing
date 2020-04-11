aa66116ff31428afc4e52252b256eb9139caf871
Timo Zhang <ztnuaa0211@gmail.com>
2020-04-11 23:35:46 +0800

    Re-write wall distance/yPlus formula in the circle ring

--- perturbURing2/perturbU.C
+++ perturbURing2/perturbU.C
@@ -68,12 +68,7 @@ int main(int argc, char *argv[])
     {
         Info<< "    Reading U" << endl;
         volVectorField U(Uheader, mesh);
-        //const scalar Retau = 300;
-        //const scalar d = 7.54/(2000.0);
-        const scalar Retau = 4384;
-        const scalar outerd = 0.0182;
-        const scalar innerd = 0.0077;
-        const scalar d=outerd-innerd;
+        const scalar Retau = 300;
 
         IOdictionary transportProperties
         (
@@ -94,7 +89,19 @@ int main(int argc, char *argv[])
         (
             transportProperties.lookup("Ubar")
         );
-  
+        dimensionedScalar diameterOuter
+        (
+            transportProperties.lookup("dOuter")
+        );
+        dimensionedScalar diameterInner
+        (
+            transportProperties.lookup("dInner")
+        );
+        
+        // Circle ring half height
+        const scalar dOuter = diameterOuter.value()/1000;
+        const scalar dInner = diameterInner.value()/1000;
+        const scalar d = (dOuter-dInner)/2;
         const scalar utau = Retau*nu.value()/d;
         //wall normal circulation
         const scalar duplus = Ubar.value()[0]*0.5/utau;
@@ -105,22 +112,29 @@ int main(int argc, char *argv[])
         //streamwise wave number: spacing x+ = 500
         const scalar alphaPlus = 2.0*constant::mathematical::pi*(1.0/800.0);
         const scalar epsilon = Ubar.value()[0]/20.0;
-        Random perturbation(1234567);
-
+        //Random perturbation(1234567);
+        
         forAll(centers, celli)
         {
-            scalar deviation=1.0 + 0.2*perturbation.GaussNormal();
-
+            //scalar deviation=1.0 + 0.2*perturbation.GaussNormal();
             scalar& Ux(U[celli].x());
             vector cCenter = centers[celli];
-            scalar rr = ::sqrt(::sqr(cCenter.y()) + ::sqr(cCenter.z()));
-            scalar r=rr;//-innerd/2;
-            scalar ringr=mag(r-(outerd+innerd)/4);
-            Ux = 1.218*mag(Ubar.value())*std::pow(max((1-2*ringr/1.01/d),SMALL),0.142857);
-            //Ux = 2*mag(Ubar.value())*(1-::sqr(r/d));
-            scalar y = d/2 - ringr;
-            r = rr*Retau/d;
-            y = y*Retau/d;
+            scalar r = ::sqrt(::sqr(cCenter.y()) + ::sqr(cCenter.z()));
+            //scalar r=rr;//-innerd/2;
+            //scalar ringr=mag(r-(outerd+innerd)/4);
+            scalar rHalf = mag(r-(dOuter+dInner)/4);
+
+            // Laminar parabolic profile
+            Ux = 2*mag(Ubar.value())*(1-::sqr(rHalf/d));
+            // Turbulent 1/7 power law profile. 
+            // Turbulent profile is  not recommend according to Dr. Eugene de Villiers
+            //Ux = 1.218*mag(Ubar.value())*std::pow(max((1-2*rHalf/1.01/d),SMALL),0.142857);
+
+            // Wall distance
+            scalar y = mag(min(dOuter/2-r,r-dInner/2));
+
+            r = r*Retau/d; //zPlus->theta*r
+            y = y*Retau/d; //yPlus
  
             scalar theta = ::atan(cCenter.y()/cCenter.z());
             scalar x = cCenter.x()*Retau/d;
@@ -128,11 +142,9 @@ int main(int argc, char *argv[])
             Ux = Ux + (utau*duplus/2.0)
                     *::cos(betaPlus*theta*r) *(y/30)
                     *::exp(-sigma*::sqr(y) + 0.5);
-            //Ux = Ux*deviation;
-            //Info<<"This is ux:"<<utau<<"\t"<<duplus<<"\t"
-            //<<betaPlus<<"\t"<<r<<"\t"<<d-ringr<<" "<<sigma<<endl;
+            //Ux = Ux*deviation; //White noise
             scalar utheta = epsilon*::sin(alphaPlus*x)*y
-                            *::exp(-sigma*::sqr(y))*100;
+                            *::exp(-sigma*::sqr(y));
             vector tangential
             (
                  0, cCenter.y(), cCenter.z()
